79321aa249383f929fe7d0c3d58c42828f2cf13c
Maxime Denis <maximedns92@gmail.com>
2026-04-27 00:17:12 -0700

    ch

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1] ?? 'Portfolio';
-
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? `/${repoName}/` : '/',
+  base: process.env.NODE_ENV === 'production' ? '/Portfolio_MD/' : '/',
 });
